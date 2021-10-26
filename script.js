bb5cc2d62bd996cbfe10b3d6ef2fd8cbdc7872af
Andreyrvs <cortaesai@gmail.com>
2021-10-26 15:46:26 -0300

    Requisito 05

---
+++ script.js
@@ -0,0 +1,6 @@
+const saveParagraph = document.querySelector('#funcionamento');
+const saveInput = document.querySelector('#texto-tarefa');
+const saveBtn = document.querySelector('#criar-tarefa');
+const saveOlist = document.querySelector('#lista-tarefas');
+
+// 
