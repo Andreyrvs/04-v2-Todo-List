ea0fec0d3ff441535fb2dd07de2539ff1549b3e8
Andreyrvs <cortaesai@gmail.com>
2021-10-26 17:02:30 -0300

    Requisito - 05

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-const saveParagraph = document.querySelector('#funcionamento');
-const saveInput = document.querySelector('#texto-tarefa');
-const saveBtn = document.querySelector('#criar-tarefa');
-const saveOlist = document.querySelector('#lista-tarefas');
+// Captura a classe pai do input e do botao
+const catchInputBtn = document.querySelector('.add-input-btn');
 
-// 
+// Captura a lista Ordana OL
+const catchOList = document.querySelector('#lista-tarefas');
+
+// Cria input
+function createInput() {
+  const createInpt = document.createElement('input');
+  createInpt.id = 'texto-tarefa';
+
+  catchInputBtn.appendChild(createInpt);
+}
+createInput();
+
+// Cria a batao
+function createButton() {
+  const createBtn = document.createElement('button');
+  createBtn.id = 'criar-tarefa';
+  createBtn.innerText = 'Nasci';
+
+  catchInputBtn.appendChild(createBtn);
+}
+createButton();
+
+// Cria as li com o que for digitado no Input
+const catchBtn = document.querySelector('#criar-tarefa');
+
+catchBtn.addEventListener('click', () => {
+  const catchInput = document.querySelector('#texto-tarefa');
+  const createLi = document.createElement('li');
+  createLi.innerText = catchInput.value;
+  catchInput.value = '';
+  catchOList.appendChild(createLi);
+
+})
