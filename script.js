b44ec73cc7591a81add7779e7902affb7c811768
Andreyrvs <cortaesai@gmail.com>
2021-10-27 13:32:39 -0300

    Estou começando do 0 pois travei no dia de ontem

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
-// Captura a classe pai do input e do botao
-const catchInputBtn = document.querySelector('.add-input-btn');
+console.log('Começouuuuuuuuuuuuu');
 
-// Captura a lista Ordana OL
-const catchOList = document.querySelector('#lista-tarefas');
+/**
+ * Um novo começo !!
+ */
+// // Captura a classe pai do input e do botao
+// const catchInputBtn = document.querySelector('.add-input-btn');
+// // Captura a lista Ordana OL
+// const catchOList = document.querySelector('#lista-tarefas');
 
-// Cria input
-function createInput() {
-  const createInpt = document.createElement('input');
-  createInpt.id = 'texto-tarefa';
+// // Cria input
+// function createInput() {
+//   const createInpt = document.createElement('input');
+//   createInpt.id = 'texto-tarefa';
 
-  catchInputBtn.appendChild(createInpt);
-}
-createInput();
+//   catchInputBtn.appendChild(createInpt);
+// }
+// createInput();
 
-// Cria a batao
-function createButton() {
-  const createBtn = document.createElement('button');
-  createBtn.id = 'criar-tarefa';
-  createBtn.innerText = 'Nasci';
+// // Cria a botao
+// function createButton() {
+//   const createBtn = document.createElement('button');
+//   createBtn.id = 'criar-tarefa';
+//   createBtn.innerText = 'Salvar';
 
-  catchInputBtn.appendChild(createBtn);
-}
-createButton();
+//   catchInputBtn.appendChild(createBtn);
+// }
+// createButton();
 
-// Cria as li com o que for digitado no Input
-const catchBtn = document.querySelector('#criar-tarefa');
+// const catchBtn = document.querySelector('#criar-tarefa');
+// function createLi() {
+//   const catchInput = document.querySelector('#texto-tarefa');
+//   const createLi = document.createElement('li');
+//   createLi.innerText = catchInput.value;
+//   catchOList.appendChild(createLi);
+//   catchInput.value = '';
+// }
 
-catchBtn.addEventListener('click', () => {
-  const catchInput = document.querySelector('#texto-tarefa');
-  const createLi = document.createElement('li');
-  createLi.innerText = catchInput.value;
-  catchOList.appendChild(createLi);
-  catchInput.value = '';
-});
+// // Cria as li com o que for digitado no Input
+
+// const catchTask = document.querySelectorAll('.task');
+
+// catchTask.addEventListener('click', (event) => {
+//   // const catchTask = document.querySelectorAll('.task');
+//   for (let index = 0; index < catchTask.length; index += 1) {
+//     catchTask[index].classList.remove('task');
+//   }
+//   event.target.classList.add('task');
+//   createLi();
+// });
+// // Troca a cor de fundo ao Clicar na Tarefa
+
+// function PegaCss() {
+//   const catchTask = document.querySelector('#lista-tarefas');
+//   const theCssprop = window
+//   .getComputedStyle(catchTask, null)
+//   .getPropertyValue('background-color');
+//   return theCssprop;
+// }
